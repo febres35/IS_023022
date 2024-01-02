6579642280fc1d7ca9c45a6e5825c3d202b3e86d
Leonardo Febres <febres35@gmail.com>
2024-01-02 17:41:02 -0400

    practica 7, ejercicio 4 , hasta el punto 3

---
+++ reto9/ejercicio4.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.Random;
+public class ejercicio4 {   
+    
+    private static boolean theWholeNumberIs_loopFor( int[] list, int var){
+        for (int i = 0; i < list.length; i++){
+            if (list[i] == var){
+                return true;
+            }
+        }
+        return false;
+    } 
+
+    private static boolean theWholeNumberIs_loopWhile( int[] list, int var){
+        int cont = 0;
+        while(cont < list.length){
+            if (list[cont] == var){
+
+                return true;
+            }
+            cont++;
+        }
+        return false;
+
+    } 
+
+    private static int fibonacciSequence(int var ){
+        int a=0, b=1;
+        for (int i = 0; i <= var; i++){
+            System.out.println(a);
+            a=b;
+            b+=a;   
+        }
+        return b;
+    } 
+
+    private static void initArray(int[] X){
+        Random random = new Random();
+        for (int i = 0; i < X.length; i++){
+            X[i] = random.nextInt(100);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+        System.out.print("indicate the size of array: ");
+        int len = entry.nextInt();
+        int[] list = new int[len];
+        initArray(list);
+        System.out.print("Search integer: ");
+        int var = entry.nextInt();
+        //boolean exists = theWholeNumberIs_loopWhile(list, var);
+        boolean exists = theWholeNumberIs_loopFor(list, var);
+        entry.close();
+        for (int x: list){
+            System.out.print(x + ", ");
+        }
+        System.out.println(" ");
+        if (exists){
+            System.out.println("El numero " + var + " Existe en el arreglo de enteros.");
+        }else{
+            System.out.println("El numero " + var + " No existe en el arreglo de enteros.");
+        }
+
+    }
+}
